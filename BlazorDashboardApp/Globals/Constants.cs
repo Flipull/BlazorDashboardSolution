6fab259fdde91fcead7436047404a7c7b6855841
Flipull <markwilemail@gmail.com>
2025-07-26 20:10:37 +0200

    Changed File destination, added max filesizes

---
+++ BlazorDashboardApp/Globals/Constants.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+
+namespace BlazorDashboardApp.Globals 
+{
+    static public class Constants
+    {
+        static public long MaxDatumUploadSize = 50 * 1024 * 1024;//50MB
+        static public long MaxPhotoUploadSize = 1 * 1024 * 1024;//1MB
+        static public string WwwRoot = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+        static public string DatumFileDirectory = Path.Combine(WwwRoot, "Data");
+        static public string PhotoFileDirectory = Path.Combine(WwwRoot, "Photos");
+
+        static public List<Char> ReservedTagCharacters = new List<Char> { '!', '+', '-', '=', '@', '#', ':' };
+        
+        static public void CreatePrerequisiteFileStructure()
+        {
+            try
+            {
+                if (!Directory.Exists(DatumFileDirectory))
+                    Directory.CreateDirectory(DatumFileDirectory);
+                if (!Directory.Exists(PhotoFileDirectory))
+                    Directory.CreateDirectory(PhotoFileDirectory);
+            }
+            catch {
+                throw;
+            }
+
+        }
+    }
+}
