d39cd40b10eb0703cc693141e9bf06213e11cf05
Flipull <markwilemail@gmail.com>
2025-07-26 20:12:47 +0200

    Added dependency objects

--- BlazorDashboardApp/Program.cs
+++ BlazorDashboardApp/Program.cs
@@ -1,10 +1,12 @@
-using BlazorDashboardApp.Components;
+﻿using BlazorDashboardApp.Components;
 using BlazorDashboardApp.Components.Account;
+using BlazorDashboardApp.Globals;
 using BlazorDashboardApp.Data;
 using BlazorDashboardApp.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using BlazorDashboardApp.Mappers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,8 +27,11 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>//variant would be builder.Services.AddDbContextFactory<>, if ever needed
     options.UseSqlServer(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -37,11 +42,33 @@ builder.Services.AddIdentityCore<IdentityUser>(options => options.SignIn.Require
 
 builder.Services.AddSingleton<IEmailSender<IdentityUser>, IdentityNoOpEmailSender>();
 
+
+/**/
+builder.Services.AddScoped<UserService>();
+
+builder.Services.AddTransient<DatumMapper>();
+builder.Services.AddTransient<DatumViewModelMapper>();
+builder.Services.AddScoped<DatumService>();
+
+builder.Services.AddTransient<TranscriptMapper>();
+builder.Services.AddTransient<TranscriptViewModelMapper>();
+builder.Services.AddScoped<TranscriptService>();
+
+builder.Services.AddTransient<TagMapper>();
+builder.Services.AddTransient<TagViewModelMapper>();
+builder.Services.AddScoped<TagService>();
+
+builder.Services.AddTransient<SubjectMapper>();
+builder.Services.AddTransient<SubjectViewModelMapper>();
+builder.Services.AddScoped<SubjectService>();
+/**/
+
+
 var app = builder.Build();
 
 
+/**/Constants.CreatePrerequisiteFileStructure();
 /**/await DbInitializer.SeedAdminUserAsync(app.Services);
-
 
 
 // Configure the HTTP request pipeline.
@@ -54,6 +81,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
